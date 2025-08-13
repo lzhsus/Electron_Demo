5b988c4b96a008dfd139f1a1d3358d45ee5a821a
lzhsus <1434253600@qq.com>
2025-08-13 18:02:33 +0800

    修改3

--- vite.config.js
+++ vite.config.js
@@ -10,51 +10,31 @@ import minify from 'vite-plugin-minify'
 import mp from 'vite-plugin-mp'
 import { ejsConfig } from './ejs.config';
 
-const Cookie  = 'doublec_session=eyJpdiI6InNqbUZQNXJkOXM3TlRCWng1Q1BiNXc9PSIsInZhbHVlIjoiYjFMMnd6QXUreStYdlNEeFZ5SGFhUnNvZ2t3S0pZcTlsYVlYMkh4Y1VaTWpqbE9qdjlOV3lCWk9GZml5VDJKZDhJUTBkVThFcUM5Ulk1UUVoRnVxSEQ1U1Evb21KZ080Ym1iU0FSbTdNRUViUGxxRVZkdzF6TFYvOWNaQnpCR2giLCJtYWMiOiJjMDQ3MWZiMzQ1YjgwMmM3MTNjOWEwMzAzNzgzZDEzZmQ4MWY3NzdmMmIxMjY3ZThhOWUwNWQ1YjIyOTg4MmVmIiwidGFnIjoiIn0%3D; path=/; secure; httponly; samesite=none';
-
 const r = p => resolve(__dirname, p)
 
-export default defineConfig({  
-  base: process.env.NODE_ENV === 'production' ? '/左面应用/electron_demo/dist/' : '/',
-  plugins: [
-    autoImport({
-      dts: r('auto-imports.d.ts'),
-      imports: ['vue'],
-    }),
-    components({
-      dirs: [r('src/common/components')],
-      dts: r('components.d.ts'),
-    }),
-    vue(),
-    mp(),
-    ejs(ejsConfig),
-    minify(),
-  ],
-  resolve: {
-    dedupe: ['vue'],
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    proxy: {
-      '/admin': {
-        target: 'https://doublec.intonecc.com',
-        changeOrigin: true,
-        headers: {
-          Cookie: Cookie
-        },
-      },
-      '/oss': {
-          target: 'https://doublec.intonecc.com',
-          changeOrigin: true,
-          headers: {
-              Cookie: Cookie
-          },
-      }
+export default defineConfig({
+    base: process.env.NODE_ENV === 'production' ? '/左面应用/electron_demo/dist/' : '/',
+    plugins: [
+        autoImport({
+            dts: r('auto-imports.d.ts'),
+            imports: ['vue'],
+        }),
+        components({
+            dirs: [r('src/common/components')],
+            dts: r('components.d.ts'),
+        }),
+        vue(),
+        mp(),
+        ejs(ejsConfig),
+        minify(),
+    ],
+    resolve: {
+        dedupe: ['vue'],
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 9999,
-  },
+    build: {
+        chunkSizeWarningLimit: 9999,
+    },
 })
